58dc4b96712d1bb7ad930afec7c0635bdf83d1c9
Gui Rios <guilherme.rc.98@gmail.com>
2026-01-20 16:02:54 -0300

    🚨 Configura lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,6 +19,20 @@ export default typescriptEslint.config(
       parserOptions: {
         parser: typescriptEslint.parser
       }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ]
     }
   }
 )
